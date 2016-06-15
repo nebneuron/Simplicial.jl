03a361dc25b7034f0c1d822ac0e4f8c4e9d2afab
Vladimir Itskov <vladimir.itskov@psu.edu>
2016-06-15 18:16:55 -0400

    added the in method for simplicial complex

--- src/utilities/function_in.jl
+++ src/utilities/function_in.jl
@@ -1,5 +1,5 @@
 
-# This function provides membership relationship of a word and a code
+# This function provides membership relationship of a word in a code
 function in(word::CodeWord,code::CombinatorialCode)
 wordlenth=length(word)
  Ind=find(code.weights.==wordlenth) # we only need to check the words of the same weight
@@ -16,3 +16,23 @@ return false
 
 # This is an array version of the function in
 in(a::Array{Int,1},c::CombinatorialCode)=in(CodeWord(a),c)
+
+
+
+# This function provides membership relationship of a word in a SimplicialComplex
+function in(word::CodeWord,code::SimplicialComplex)
+word_dimension=length(word)-1;
+ Ind=find(K.dimensions.>=word_dimension) # we only need to check the words of dimension bigger or equal to the dimension of the word
+ 	if length(Ind)<1
+ 		return false
+ 	else  for j=1:length(Ind)
+ 		      if issubset(word,code.facets[Ind[j]])
+ 		      	  return true
+ 		      	end
+ 		    end
+ 	end
+return false
+ end
+
+ # This is an array version of the function in
+ in(a::Array{Int,1},K::SimplicialComplex)=in(CodeWord(a),K)
