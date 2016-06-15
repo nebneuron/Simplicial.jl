548d8a417cb71e175c87397783dcc81c84b2c045
Vladimir Itskov <vitskov@users.noreply.github.com>
2016-06-15 15:26:32 -0400

    Add files via upload

---
+++ Simplicial.jl
@@ -0,0 +1,31 @@
+module Simplicial
+import Base.in # since we define a new method for the function in, we need to load all the old methods -- this is an oddity of the current version of julia
+import Base.==
+
+include("CodeWords.jl") # definition for the type of CodeWord and the related methods for this type
+
+include("CombinatorialCodes/CombinatorialCodes.jl")
+include("CombinatorialCodes/BernoulliRandomCode.jl")
+
+include("SimplicialComplexes/SimplicialComplex.jl")
+include("utilities/function_in.jl")     # This is a simple function that checks a codeword membership in a code. Needs also be written for the simplicial complex type..
+include("utilities/SC2CC_and_CC2SC.jl") # These are functions that transform between the SimplicialComplex and CombinatorialCode types
+
+include("utilities/CC_and_SC_compare_functions.jl")
+
+include("SimplicialComplexes/Alexander_dual_function.jl")
+
+include("utilities/link_function_SC_and_CC.jl")
+
+include("utilities/del_function_SC_and_CC.jl")
+
+include("utilities/function_Bicomplex.jl")
+
+include("utilities/function_AIMC_minus_C_and_link_AIMC_minus_C.jl")
+
+export CodeWord, emptyset
+export CombinatorialCode, BernoulliRandomCode, HasEmptySet, isNULL, in, ==
+export SimplicialComplex, Alexander_dual, link, del, Bicomplex
+export  AIMC_minus_C, link_AIMC_minus_C
+
+end
