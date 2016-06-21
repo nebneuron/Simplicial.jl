5a6a3c79ac3c54e5324ea32845f4ad1acc3202b9
Alex Kunin <abk170@psu.edu>
2016-06-21 16:55:40 -0400

    New directory structure/filenames
    Part of a large change to the repository where I change a bunch of file names and changed a bunch of types - the big one is changing SimplicialComplex into an abstract type and changing the current concrete class into FacetList.

--- src/Simplicial.jl
+++ src/Simplicial.jl
@@ -1,33 +1,35 @@
 module Simplicial
 import Base.in # since we define a new method for the function in, we need to load all the old methods -- this is an oddity of the current version of julia
 import Base.==
-import Base.show
+
+# Define the parent abstract types
+abstract FiniteSetCollection
+abstract SimplicialComplex <: FiniteSetCollection
 
 include("CodeWords.jl") # definition for the type of CodeWord and the related methods for this type
 
-include("CombinatorialCodes/CombinatorialCodes.jl")
-include("CombinatorialCodes/BernoulliRandomCode.jl")
+include("CombinatorialCode/CombinatorialCode.jl")
+include("CombinatorialCode/BernoulliRandomCode.jl")
 
-include("SimplicialComplexes/SimplicialComplex.jl")
-include("utilities/function_in.jl")     # This is a simple function that checks a codeword membership in a code.
+include("SimplicialComplex/FacetList.jl")
+include("utilities/function_in.jl")     # This is a simple function that checks a codeword membership in a code. Needs also be written for the simplicial complex type..
 include("utilities/SC2CC_and_CC2SC.jl") # These are functions that transform between the SimplicialComplex and CombinatorialCode types
 
-include("utilities/CC_and_SC_compare_functions.jl")
+include("utilities/function_equalequal.jl")
 
-include("SimplicialComplexes/Alexander_dual_function.jl")
+include("utilities/function_dual.jl")
 
-include("utilities/link_function_SC_and_CC.jl")
+include("utilities/function_link.jl")
 
-include("utilities/del_function_SC_and_CC.jl")
+include("utilities/function_deleteFace.jl")
 
 include("utilities/function_Bicomplex.jl")
 
 include("utilities/function_AIMC_minus_C_and_link_AIMC_minus_C.jl")
-include("utilities/function_show.jl") # This is a function for dysplaying the underlying objects. Currently needs to be expanded to all the types
 
 export CodeWord, emptyset
-export CombinatorialCode, BernoulliRandomCode, HasEmptySet, isNULL, in, ==
-export SimplicialComplex, Alexander_dual, link, del, Bicomplex
+export CombinatorialCode, BernoulliRandomCode, HasEmptySet, isNull, isVoid, in, ==
+export FacetList, Alexander_dual, link, del, Bicomplex
 export  AIMC_minus_C, link_AIMC_minus_C
-export show
+
 end
