05202878b6e47e3e134195d6fdfbcfbb2b6c0789
Alex Kunin <abk170@psu.edu>
2017-08-08 14:19:23 -0400

    fixed constructor issues, added iteration interface (#4)
    * CombinatorialCode constructor fix
    
    now accepts any 1-d array
    
    * SimplicialComplexes constructor fix
    
    now accepts any 1-d array, no need for two identical constructors
    
    * iteration interface for CombinatorialCode
    
    allows CombinatorialCode to be used anywhere a generic "for c in C"
    type loop would be used
    
    * iteration interface for SimplicialComplex
    
    allows for iteration over *facets* of a simplicial complex
    
    * combo code from simp cx now includes empty set
    
    * add eltype(Type) methods
    
    * link w.r.t. "negative space"
    
    Allows for computing link of code with respect to ``signed set''

--- src/CombinatorialCodes/CombinatorialCodes.jl
+++ src/CombinatorialCodes/CombinatorialCodes.jl
@@ -1,61 +1,61 @@
 ###################### This is the type that defines combinatorial codes
 type CombinatorialCode
-                words::Array{CodeWord,1}   # the codewords, these are ordered by the weights (in the increasing order)
-                weights::Array{Int,1} # the sizes of the codewords in the same order as the words
-                MaximumWeight::Int  #
-                Minimumweight::Int  #
-                Nwords::Int       	# total number of codewords in the code
-                vertices::CodeWord 	# the set of all vertices that show up in the code
+  words::Array{CodeWord,1}   # the codewords, these are ordered by the weights (in the increasing order)
+  weights::Array{Int,1} # the sizes of the codewords in the same order as the words
+  MaximumWeight::Int  #
+  Minimumweight::Int  #
+  Nwords::Int       	# total number of codewords in the code
+  vertices::CodeWord 	# the set of all vertices that show up in the code
   ## this is the constructor for the CombinatorialCode type. It takes a list of Integer arrays, where each array represents a codeword
   ## codewords are checked for duplication
 
   # The following function constructs a combinatorial code from a list of words
-  function CombinatorialCode(ListOfWords::Array{Any,1})
-           if length(ListOfWords)<1; println("WARNING: The void code was passed!!");
-               return new([], Array{Int,1}([]),-1,-1,0,emptyset)
-           end
-           vertices=emptyset # keep track of all the vertices here
-           # First, compute the weights
-           weights=zeros(Int,length(ListOfWords))
-           for i=1:length(ListOfWords)
-            weights[i]=length(unique(ListOfWords[i]))
-           end
-           MaximumWeight=maximum(weights)
-           Minimumweight=minimum(weights)
-           # next we figure out if there are any duplicate words in the list and populate the list of sets named words
-           words=CodeWord[];
-           ThereWereDuplicates=false
-           for w= Minimumweight:MaximumWeight
-               Ind=find(weights.==w);
-               L=length(Ind);
-                if L>0
-               	   CurrentListOfWords=CodeWord[];
-               # if there was more than one word with the same weights we check the repeated words
-               	  for j=1:L
-               	  	    CurrentSet=CodeWord((ListOfWords[Ind[j]]))
-                        CurrentSetIsDuplicated=false
-                        for k=1:length(CurrentListOfWords) # check the previous words of the same weight is duplicated, if yes, then discard the CurrentSet
-                        	if CurrentListOfWords[k]==CurrentSet
-                        		    CurrentSetIsDuplicated=true
-                                ThereWereDuplicates=true
-                        		break
-                        	end
-                        end
-                        if ~CurrentSetIsDuplicated
-                        push!(CurrentListOfWords,CurrentSet)
-                        push!(words,CurrentSet)
-                        vertices=union(vertices,CurrentSet)
-                        end
-               	    end
-                 end
-            end
-            Nwords=length(words)
-            # now we recompute the weights for possibly reduced list, since we removed all the duplicates
-            weights=zeros(Int,Nwords)
-            for i=1:Nwords ; weights[i]=length(words[i]); end
-           if ThereWereDuplicates println("Warning: There were duplicated words") end
-           new(words,weights,MaximumWeight,Minimumweight,Nwords,vertices)
+  function CombinatorialCode(ListOfWords::Vector)
+    if length(ListOfWords)<1; println("WARNING: The void code was passed!!");
+      return new([], Array{Int,1}([]),-1,-1,0,emptyset)
+    end
+    vertices=emptyset # keep track of all the vertices here
+    # First, compute the weights
+    weights=zeros(Int,length(ListOfWords))
+    for i=1:length(ListOfWords)
+    weights[i]=length(unique(ListOfWords[i]))
+    end
+    MaximumWeight=maximum(weights)
+    Minimumweight=minimum(weights)
+    # next we figure out if there are any duplicate words in the list and populate the list of sets named words
+    words=CodeWord[];
+    ThereWereDuplicates=false
+    for w= Minimumweight:MaximumWeight
+      Ind=find(weights.==w);
+      L=length(Ind);
+      if L>0
+        CurrentListOfWords=CodeWord[];
+        # if there was more than one word with the same weights we check the repeated words
+        for j=1:L
+          CurrentSet=CodeWord((ListOfWords[Ind[j]]))
+          CurrentSetIsDuplicated=false
+          for k=1:length(CurrentListOfWords) # check the previous words of the same weight is duplicated, if yes, then discard the CurrentSet
+          	if CurrentListOfWords[k]==CurrentSet
+              CurrentSetIsDuplicated=true
+              ThereWereDuplicates=true
+          		break
+          	end
+          end
+          if ~CurrentSetIsDuplicated
+          push!(CurrentListOfWords,CurrentSet)
+          push!(words,CurrentSet)
+          vertices=union(vertices,CurrentSet)
+          end
         end
+      end
+    end
+    Nwords=length(words)
+    # now we recompute the weights for possibly reduced list, since we removed all the duplicates
+    weights=zeros(Int,Nwords)
+    for i=1:Nwords ; weights[i]=length(words[i]); end
+    if ThereWereDuplicates println("Warning: There were duplicated words") end
+    new(words,weights,MaximumWeight,Minimumweight,Nwords,vertices)
+  end
 
 
 """
@@ -77,6 +77,29 @@ type CombinatorialCode
 end
 
 ###############################################################################################
+
+#################
+# ITERATION
+#################
+function start(CC::CombinatorialCode)
+  return 1
+end
+function next(CC::CombinatorialCode, state)
+  return (CC.words[state], state+1)
+end
+function done(CC::CombinatorialCode, state)
+  return state > length(CC.words)
+end
+function eltype(::CombinatorialCode)
+  return CodeWord
+end
+function eltype(::Type{CombinatorialCode})
+  return CodeWord
+end
+function length(CC::CombinatorialCode)
+  return length(CC.words)
+end
+
 
 
 # This is a function that detects if the code has the empty set:
--- src/utilities/link_function_SC_and_CC.jl
+++ src/utilities/link_function_SC_and_CC.jl
@@ -27,24 +27,41 @@ end
 #####################################################################################################
 
 ## this function computes the link of a sub-codeword in a combinatorial code
-function link(C::CombinatorialCode, sigma::Set{Int})
-    ## for testing validity of sigma,
-    ## build an array Codeword_test, if sigma is contained in a facet, push the facet into the array
-    Codeword_test=[]
-    for i=1:C.Nwords
-        if issubset(sigma,C.words[i])
-                push!(Codeword_test, C.words[i])
-        end
-    end
+# function link(C::CombinatorialCode, sigma::Set{Int})
+#     ## for testing validity of sigma,
+#     ## build an array Codeword_test, if sigma is contained in a facet, push the facet into the array
+#     Codeword_test=[]
+#     for i=1:C.Nwords
+#         if issubset(sigma,C.words[i])
+#                 push!(Codeword_test, C.words[i])
+#         end
+#     end
+#
+#     ## if Simplex_test is empty, sigma is not a simplex
+#     ## else do setdiff for each collected facet, giving link
+#     if Codeword_test==[]
+#         println("ERROR!! $sigma is not a sub-codeword.")
+#     else
+#         for i=1:length(Codeword_test)
+#             Codeword_test[i]=collect(setdiff(Codeword_test[i],sigma))
+#         end
+#         CombinatorialCode(Any(Codeword_test))
+#     end
+# end
 
-    ## if Simplex_test is empty, sigma is not a simplex
-    ## else do setdiff for each collected facet, giving link
-    if Codeword_test==[]
-        println("ERROR!! $sigma is not a sub-codeword.")
-    else
-        for i=1:length(Codeword_test)
-            Codeword_test[i]=collect(setdiff(Codeword_test[i],sigma))
-        end
-        CombinatorialCode(Any(Codeword_test))
-    end
+"""
+    link(C::CombinatorialCode, sigma, tau=[])
+
+Computes the link of `C` with respect to `sigma`. If `tau` is nonempty, only
+returns codewords disjoint from tau. If no codewords `c` satisfy `sigma ⊆ c` and
+`tau ∩ c = ∅`, prints a warning message and does not return any value.
+
+"""
+function link(C::CombinatorialCode, sigma, tau=[])
+  new_words = filter(c -> issubset(sigma, c) && (isempty(tau) || isempty(intersect(tau, c))), C)
+  if isempty(new_words)
+    println("ERROR!! $sigma is not a sub-codeword, and/or ($sigma,$tau) is a combinatorial relation")
+  else
+    return CombinatorialCode([setdiff(c, sigma) for c in new_words])
+  end
 end
