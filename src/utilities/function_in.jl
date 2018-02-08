5b5651f24d1c5ea3195dc06e5d9e0c6e2503bb9b
Vladimir Itskov <vitskov@users.noreply.github.com>
2018-02-08 17:44:35 -0500

    Update function_in.jl
    function  in(word::DirectedCodeword,D::DirectedComplex)::Bool

--- src/utilities/function_in.jl
+++ src/utilities/function_in.jl
@@ -36,3 +36,16 @@ return false
 
  # This is an array version of the function in
  in(a::Array{Int,1},K::SimplicialComplex)=in(CodeWord(a),K)
+
+
+# This is the sequence membership function 
+function  in(word::DirectedCodeword,D::DirectedComplex)::Bool
+word_dimension=length(word)-1; Ind=find(D.dimensions.>=word_dimension); # we only need to check the facets of dimension bigger or equal to the dimension of the word
+  if length(Ind)<1; return false
+ 	else  for j=1:length(Ind); if (word<=D.facets[Ind[j]]); return true; end; end
+ 	end
+return false # if we finished the loop, then the sequence is not in the complex
+end
+
+# This is the integer array version of the same function: 
+in(a::Array{Int,1},D::DirectedComplex)=in(DirectedCodeword(a),D)::Bool
