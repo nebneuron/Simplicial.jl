404b8aa895ecb2e9c56ca228d74904dcef3535da
Alex Kunin <abk170@psu.edu>
2018-03-21 13:17:02 -0400

    cosmetic: removed commented-out obsolete code

--- src/utilities/function_Bicomplex.jl
+++ src/utilities/function_Bicomplex.jl
@@ -1,25 +1,12 @@
 
 export polar_complex, Bicomplex
 
-# """ This function computes the bicomplex of a combinatorial code
-# Usage: BDelta= Bicomplex(C);
-# """
-# function Bicomplex(CC::CombinatorialCode)
-#     ## Collect the union of original codewords and its complement's dual
-#   NewWords=  Array{Array{Int,1},1}([]);
-#     for i=1:CC.Nwords
-#         push!(NewWords, union(collect(CC.words[i]),  -collect(setdiff(CC.vertices,CC.words[i]))))
-#     end
-#     return SimplicialComplex(NewWords)
-# end
-
 """
     polar_complex([FacetList], C)
 
 Constructs the polar complex ``Γ(C)`` for the given code.
 """
 function polar_complex(::Type{T}, C::AbstractCombinatorialCode) where {T<:AbstractAbstractSimplicialComplex}
-    #TODO polar complex from combinatorial code
     V = [vertices(C); -vertices(C)]
     F = [union(c, -setdiff(vertices(C), c)) for c in C]
     # Temporary bugfix: Check for FacetList type explicitly
