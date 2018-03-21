643b355b6a8411007d7008b33e3a6cfffe3d740e
Alex Kunin <abk170@psu.edu>
2018-03-21 12:32:58 -0400

    prettyprint display for canonical form
    Also removed lots of unused functions from function_show.jl. There is
    only one function left in there which will be moved to the relevant
    location and then the file will be removed

--- src/CombinatorialCodes/NeuralRing.jl
+++ src/CombinatorialCodes/NeuralRing.jl
@@ -1,13 +1,6 @@
 
-export SmallIntegerType, PseudoMonomial, CanonicalForm, Code2CF,
-	PseudoMonomialType,
+export PseudoMonomial, PseudoMonomialType, CanonicalForm,
 	canonical_form
-
-
-"below are constants used in the canonical form computation. In the future, these will be obsolete"
-const SmallIntegerType=Int8;
-const positive_one= SmallIntegerType(1);
-const negative_one= SmallIntegerType(-1);
 
 """
     PseudoMonomial
@@ -29,213 +22,57 @@ struct PseudoMonomial
         new(find(b[1:length(b)>>1]), find(b[1+length(b)>>1:end]))
     end
 end
+
 function show(io::IO, pm::PseudoMonomial)
-	# print(io, join([join(string.("x_",pm.x), " ") join(string.("(1-x_",pm.y,")"), "")], " "))
-    print(io, join(string.("x_", pm.x), " "))
-    if length(pm.x) > 0 && length(pm.y) > 0
-        print(io, " ")
+	if isempty(pm.x) && isempty(pm.y)
+        print(io, "1")
+    else
+        print(io, join(string.("x_", pm.x), " "))
+        if !isempty(pm.x) && !isempty(pm.y)
+            print(io, " ")
+        end
+        print(io, join(string.("(1-x_", pm.y ,")")))
     end
-    print(io, join(string.("(1-x_", pm.y ,")")))
 end
+
 ==(pm1::PseudoMonomial, pm2::PseudoMonomial) = pm1.x == pm2.x && pm1.y == pm2.y
 
 
 """
-function PseudoMonomialType(p::PseudoMonomial)::Int
-This function returns the type of a pseudomonomial
-according to the classification in the neural ring paper
+    PseudoMonomialType(p::PseudoMonomial)
 
+Return the type of `p` as classified in [the neural ring
+paper](https://arxiv.org/abs/1212.4201) as an `Int`.
 
+Type I:   ``σ ≠ ∅, τ = ∅``
+Type II:  ``σ ≠ ∅, τ ≠ ∅``
+Type III: ``σ = ∅, τ ≠ ∅``
 """
-function PseudoMonomialType(p::PseudoMonomial)::Int
- l1=isempty(p.x); l2= isempty(p.y)
- if l1 && l2;  return 0; end
- if l1 && !l2; return 3; end
- if !l1 && l2; return 1
- else return 2
- end
+function PseudoMonomialType(p::PseudoMonomial)
+    l1=isempty(p.x); l2= isempty(p.y)
+    if l1 && l2;  return 0; end
+    if l1 && !l2; return 3; end
+    if !l1 && l2; return 1
+    else return 2
+    end
 end
 
-
-" CanonicalForm is a type used for encoding canonical forms. It is a 1-dimensional array of pseudomonomials"
-const CanonicalForm=Array{PseudoMonomial,1}
-
-"""
-     CanonicalForm(C::CombinatorialCode)::CanonicalForm
-     This computes the canonical form of a neural ideal for the combinatorial code C
-     Example Usage:   CF=CanonicalForm(C)
-"""
-function CanonicalForm(C::AbstractCombinatorialCode)::CanonicalForm
-    # First, convert to the binary representation:
-    if isvoid(C)
-        error("This code is void, i.e. does not contain any codewords")
+# custom display for arrays of pseudomonomials
+function show(io::IO, CF::Array{PseudoMonomial,1})
+    if length(CF) == 0
+        get(io, :compact, false) ?
+            show(io, "Empty canonical form") :
+            show(io, "This canonical form is empty; i.e. the corresponding code consists of all subsets of [n]")
     else
-        # BA=BitArrayOfACombinatorialCode(C)
-        # return Code2CF(BA.BinaryMatrix)
-        return Code2CF(matrix_form(C))
-    end
-end
-
-
-"""   This computes the canonical form of a neural ideal for the combinatorial code C, represented by a bit array  whose rows are codewords.
-      The algorithm is an optimized Julia language  version of the matlab code in https://github.com/nebneuron/neural-ideal/blob/master/Code2CF.m
-      that was originally written  by Nora Youngs.
-      For more information about the neural ideal and its uses, see e.g. the paper about the neural ring http://arxiv.org/abs/1212.4201
-
-"""
-function Code2CF(C::BitArray{2})::CanonicalForm
-    # ----------------------------------------------------------------------
-    # Loops through intersecting ideals until we have the canonical form.
-    #
-    # INPUT:
-    # C: a binary matrix whose rows are codewords, of length n
-
-    # OUTPUT:
-
-    # M: the matrix form storing canonical form information. (Optional)
-    #       Each row represents a pseudo-monomial;  1 in the ith position
-    #       (for i <= n) indicates x_i is a factor; 1 in the ith position
-    #       (for i>n) indicates (1-x_{i-n}) is a factor.
-    #
-    # t: the number of operations it performed, used for time complexity
-    # analysis (Not Currently Used)
-
-    Nrows,Ncolumns=size(C);
-
-    if Nrows==0; error("This code is void, i.e. does not contain any codewords"); end
-    # Now check if this code consists of exactly one codeword. If so return the appropriate canonical form
-    if Nrows==1;
-      CF=CanonicalForm(Ncolumns);
-      for i=1:Ncolumns
-          if C[1,i]
-                CF[i]=PseudoMonomial(CodeWord([]),CodeWord([i]));
-
-          else
-                CF[i]=PseudoMonomial(CodeWord([i]),CodeWord([]));
-          end
-      end
-      return CF
-    end
-
-    # First, we make a matrix L whose values are =5 off diagonal and =C[1,j] on the j-th element of the main diagonal
-    L=fill(SmallIntegerType(5),(Ncolumns,Ncolumns));
-    for j=1:Ncolumns; L[j,j] =C[1,j]? 1:0; end
-
-    I=FinalIntersectIdeals(L,map(SmallIntegerType,C[2,:]));
-
-    for i=3:Nrows
-        I=FinalIntersectIdeals(I,  map(SmallIntegerType, C[i,:])); #compute repeated intersections
-    end
-
-    # Finally we rewrite the output as an array of pseudomonomials
-    a,b=size(I);     # Here we rewrite the final information to
-    CF=CanonicalForm(a);
-    for i=1:a
-        x=CodeWord([]); y=CodeWord([]);
-        for j=1:b
-            if I[i,j]==1
-                 push!(y,TheIntegerType(j))
-            elseif I[i,j]==0
-                push!(x,TheIntegerType(j))
-            end
-        end #   for j=1:b
-        CF[i]=PseudoMonomial(x,y);
-    end
-    return  CF
-end
-
-
-function  FinalIntersectIdeals(L::Array{SmallIntegerType,2},r::Array{SmallIntegerType,1})
-    #-------------------------------------------------------------------------
-    # This function takes a L, a matrix with rows representing
-    # the pseudo-monomials of an ideal and r, a vector representing the prime ideal
-    # for one codeword,
-    # and intersects them assuming the ideals are binary. In the matrices
-    # xi is represented by 0, (1-xi) is represented by 1,
-    # and if neither term appears, that is represented with a 5.
-
-    # Ideal is the resulting intersected ideal, and t is the number of
-    # operations that occurred.
-    #-------------------------------------------------------------------------
-
-
-    n = size(L,2);
-    m = size(L,1);
-    I = fill(SmallIntegerType(5),(m*n,n));
-    k=0; # number of ideals in L to keep in I
-
-    L0=spzeros(SmallIntegerType,m,n);
-    w=0; # This counts the number of "meaningfull rows" in L0, i.e. the number of ideals to intersect
-
-    # if any element in any row of L matches a monomial in r, put that row in I
-    # otherwise, multiply it by each monomial in r in the next step
-    for i=1:m
-        # check if any element in the given row of L matches a monomial in r
-          Match=false;
-          for j=1:n
-              l=L[i,j]
-              if l!=5 && l==r[j]
-                   Match=true;
-                   break
-              end
-          end
-        if Match
-            k+=1;
-            I[k,:]=L[i,:];
-
+        if get(io, :compact, false)
+            show(io, "{$(join(CF, ",  "))}")
         else
-            w+=1;
-            L0[w,:]=L[i,:];
+            println(io, "This is a canonical form of $(length(CF)) pseudomonomials:")
+            println(io, "{Type I:   $(join(filter(p -> PseudoMonomialType(p) == 1, CF), ",  "))")
+            println(io, " Type II:  $(join(filter(p -> PseudoMonomialType(p) == 2, CF), ",  "))")
+            println(io, " Type III: $(join(filter(p -> PseudoMonomialType(p) == 3, CF), ",  ")) }")
         end
     end
-    k1=k; # number of ideals in L to keep in I
-    k+=1;
-
-
-    # go through and multiply each pseudomon in L0 by each prime in r
-    # unless it is a multiple of a prime already in I
-    for i=1:w
-        for j=1:n
-            if L0[i,j]==5   #     otherwise, we get xi[1-xi]
-                I[k,:]=L0[i,:]; # put the i-th row of L0 in the new ideal
-                I[k,j]=r[j];    # multiply it by the j-th monomial of r
-                if k>0
-                    M=false; # is it a multiple of something?
-                    for l=1:k1;
-                        # the_diff = I[l,:]-I[k,:];
-                        condition_flag=true; # this is the condition all(abs(the_diff).!=1)
-                           for pos=1:n
-                               a=(I[l,pos]-I[k,pos])
-                               if  a==positive_one
-                                   condition_flag=false; break;
-                               elseif  a==negative_one
-                                   condition_flag=false; break;
-                               end
-                           end
-                        if condition_flag #if we do not have xi and 1-x, i.e. none of the entries of abs(the_diff) is equal to 1.
-                                condition2=true;
-                                for pos=1:n
-                                     if  (I[l,pos]-I[k,pos])<negative_one
-                                         condition2=false; break;
-                                       end
-                                end
-                            if condition2 # all(the_diff.>=-1) #and there are no extra terms in the old ideal
-                                M=true;   # then yes, this is a multiple of something in our list
-                                break;    # definitely not going to put it in, so just stop
-                            end
-                        end
-                    end
-                    if M==false #no multiples, move on
-                        k+=1;
-                    end
-                else # if k is 1 or 2,
-                    k+=1;
-                end
-            end
-        end
-    end
-    Ideal = I[1:k-1,:];
-    return Ideal
 end
 
 """
@@ -243,7 +80,8 @@ end
 
 The set of minimal psuedomonomials that generate the ideal ``I_C``. See [The
 Neural Ring](https://arxiv.org/abs/1212.4201). Per the conventions of that
-paper, this set excludes the "boolean relations" ``x_i(1-x_i)``.
+paper, this set excludes the "boolean relations" ``x_i(1-x_i)``. Returns a
+`Vector` of [`PseudoMonomial`](@ref)s
 
 This is an implementation of Algorithm 1 from [this
 paper](https://arxiv.org/abs/1511.00255)
@@ -298,3 +136,6 @@ function double_diagonal(b)
     M[n^2+1:(n+1):2n^2] = b[n+1:2n]
     return M
 end
+
+# DEPRECATED
+CanonicalForm(args...) = canonical_form(args...)
--- src/ImportantConstants.jl
+++ src/ImportantConstants.jl
@@ -9,8 +9,8 @@ export CodeWord, emptyset, TheIntegerType,
 "  TheIntegerType is the integer type that is used for enumerating the vertices of combinatorial codes and simplicial complexes"
 TheIntegerType=Int16 #
 
-" CodeWord us the type ised to encode sets of vertices (used throughout this package). Currently,  CodeWord=Set{TheIntegerType}"
-CodeWord=Set{TheIntegerType}  # We currently encode sets via sparse sets of signed integers -- this optimizes memory usage, but not speed
+" CodeWord is the type ised to encode sets of vertices (used throughout this package). Currently,  CodeWord=Set{TheIntegerType}"
+const CodeWord = Set{TheIntegerType}  # We currently encode sets via sparse sets of signed integers -- this optimizes memory usage, but not speed
 # We could have used different methods of defining sets in Julia.
 # For example we could have used IntSet, that would have optimized speed over memory...
 # Another sensible option might be the sparse boolean arrays (in this case the subset, in and some other "elementary" functions would have to be re-written to work with this type)
@@ -18,6 +18,15 @@ CodeWord=Set{TheIntegerType}  # We currently encode sets via sparse sets of sign
 
 " emptyset is the representation of the emptyset of the type CodeWord, i.e. emptyset=CodeWord([]) "
 const emptyset=CodeWord([]) # This definition should agree with the CodeWord type
+
+" Show a single CodeWord"
+function show(io::IO, c::CodeWord)
+    if isempty(c)
+        print(io, "emptyset")
+    else
+        print(io, join(sort(collect(c)), " "))
+    end
+end
 
 
 " MaximalHomologicalDimension=8 This is the maximal homological dimension allowed by certain memory-intensive  methods that are computing too many faces. This is used as a precaution against crushing when demanding too much memory"
