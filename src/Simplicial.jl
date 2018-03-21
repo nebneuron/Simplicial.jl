60bee85695e408315f7937612964fa9021cd20b6
Alex Kunin <abk170@psu.edu>
2018-03-21 13:18:07 -0400

    clean up include statement
    avoiding path delimeters for platform independence (i.e. "foo/bar"
    might cause problems on windows)

--- src/Simplicial.jl
+++ src/Simplicial.jl
@@ -9,37 +9,31 @@ import Base: in, ==, <=, show, push!, transpose,
 
 map(include,
     ["ImportantConstants.jl", # definition for the type of CodeWord and the related methods for this type
-     joinpath("utilities","auxiliaryoperations.jl"),
-     "CombinatorialCodes/CombinatorialCodes.jl",
-     joinpath("CombinatorialCodes","CodeOperations.jl"),
-     "CombinatorialCodes/BernoulliRandomCode.jl",
-     "CombinatorialCodes/NeuralRing.jl",
-     "SimplicialComplexes/SimplicialComplex.jl",
-     "SimplicialComplexes/ExampleSimplicialComplexes.jl", # Some examples of simplicial complexes
-     "utilities/cham.jl",
-     # "utilities/is_void_or_irrelevant.jl",          # This defines functions isvoid and isirrelevant on simplicial complexes and codes
-     # "utilities/function_in.jl",     # This is a simple function that checks a codeword membership in a code.
-     # "utilities/SC2CC_and_CC2SC.jl", # These are functions that transform between the SimplicialComplex and CombinatorialCode types
-     # "utilities/CC_and_SC_compare_functions.jl", # == operator defined together with types
-     "SimplicialComplexes/Alexander_dual_function.jl",
-     # "utilities/link_function_SC_and_CC.jl",
-     # "utilities/del_function_SC_and_CC.jl",
-     "utilities/DeleteRedundantFacets!.jl",
-     "utilities/function_Bicomplex.jl",
-     "utilities/void_and_irrelevant_complexes_and_codes.jl",
-     "utilities/function_AIMC_minus_C_and_link_AIMC_minus_C.jl",
-     "SimplicialComplexes/FiltrationOfSimplicialComplexes.jl",
-     "HomologyComputations/PersistenceIntervals.jl",
-     "utilities/function_show.jl", # This is a function for dysplaying the underlying objects. Currently needs to be expanded to all the types
-     "plotting/PlottingFunctions.jl",
-     "utilities/fvector.jl",
-     "DirectedComplexes/DirectedComplex.jl", "DirectedComplexes/Posets.jl"
+     joinpath("utilities", "auxiliaryoperations.jl"),
+     joinpath("CombinatorialCodes", "CombinatorialCodes.jl"),
+     joinpath("CombinatorialCodes", "CodeOperations.jl"),
+     joinpath("CombinatorialCodes", "BernoulliRandomCode.jl"),
+     joinpath("CombinatorialCodes", "NeuralRing.jl"),
+     joinpath("SimplicialComplexes", "SimplicialComplex.jl"),
+     joinpath("SimplicialComplexes", "ExampleSimplicialComplexes.jl"), # Some examples of simplicial complexes
+     joinpath("utilities", "cham.jl"),
+     joinpath("utilities", "fvector.jl"),
+     joinpath("SimplicialComplexes", "Alexander_dual_function.jl"),
+     joinpath("utilities", "DeleteRedundantFacets!.jl"),
+     joinpath("utilities", "function_Bicomplex.jl"),
+     joinpath("utilities", "function_AIMC_minus_C_and_link_AIMC_minus_C.jl"),
+     joinpath("SimplicialComplexes", "FiltrationOfSimplicialComplexes.jl"),
+     joinpath("HomologyComputations", "PersistenceIntervals.jl"),
+     joinpath("utilities", "function_show.jl"), # most show functions have been moved next to the type declarations. This file will be removed at next version.
+     joinpath("plotting", "PlottingFunctions.jl"),
+     joinpath("DirectedComplexes", "DirectedComplex.jl"),
+     joinpath("DirectedComplexes", "Posets.jl")
      ])
 
 # For ease of reading/updating, export statements are being moved to the files
 # where the types/methods are defined.
 export in, ==
-export BettiNumbers, VoidComplex, IrrelevantComplex, Alexander_dual, DeleteRedundantFacets!
+export BettiNumbers, Alexander_dual, DeleteRedundantFacets!
 export FiltrationOfSimplicialComplexes, Skeleton, Skeleton_OLD, PersistenceIntervals, Intervals2Bettis, DowkerComplex, DowkerPersistentintervals, Sample
 export AIMC_minus_C, link_AIMC_minus_C
 export show
