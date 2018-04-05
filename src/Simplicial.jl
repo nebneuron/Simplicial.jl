0c100b3f5f10ea0a4c1cb527d9649e87778e412c
Vladimir Itskov <vitskov@users.noreply.github.com>
2018-04-04 22:48:46 -0400

    Update Simplicial.jl

--- src/Simplicial.jl
+++ src/Simplicial.jl
@@ -1,6 +1,6 @@
 __precompile__(true)
 module Simplicial
-using Combinatorics, LightGraphs, Plotly 
+using Combinatorics, LightGraphs 
 import Base.in, Base.==, Base.<=, Base.show, Base.push!, Base.transpose
 import Base: start, next, done, eltype, length
 map(include,
