29e0e3c93de7a9cc2c93b3bea9b61b6523291bee
Vladimir Itskov <vitskov@users.noreply.github.com>
2018-08-12 11:20:31 -0400

    Update CodeOperations.jl
    replaced escape sequences in comments
    \{ => \\{ 
    \} => \\}

--- src/CombinatorialCodes/CodeOperations.jl
+++ src/CombinatorialCodes/CodeOperations.jl
@@ -17,9 +17,9 @@ end
 
 Compute the union product of codes `C1` and `C2`, under the GrRevLex ordering on both codes.
 
-For ``A = \{a_1,...,a_m\}`` a code on ``[d]`` and ``B = \{b_1,...,b_d\}`` a code on ``[n]``, the
+For ``A = \\{a_1,...,a_m\\}`` a code on ``[d]`` and ``B = \\{b_1,...,b_d\\}`` a code on ``[n]``, the
 union product is a code on ``[n]`` defined by
-``A ∨ B = \{ ⋃_{j ∈ a_i} b_j : a_i ∈ A\}``
+``A ∨ B = \\{ ⋃_{j ∈ a_i} b_j : a_i ∈ A\\}``
 
 More simply, this is the code obtained from the nonzero entries of the product of the matrix
 forms of ``A`` and ``B``.
