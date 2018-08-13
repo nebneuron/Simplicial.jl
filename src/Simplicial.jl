7ff9d41934f026bf46afa4dcfd80d6d963cf855e
Vladimir Itskov <vitskov@users.noreply.github.com>
2018-08-12 21:02:38 -0400

    Update Simplicial.jl
    julia 1.0 curse ..

--- src/Simplicial.jl
+++ src/Simplicial.jl
@@ -16,7 +16,7 @@ import Base: in, ==, <=, >=, <, >, show, push!, transpose,
     # we introduce these backward compartibility fixes below
  if VERSION>= v"0.7.0"
        const PATHOF_Simplicial=dirname(pathof(Simplicial))
-       using DelimitedFiles
+       using DelimitedFiles, SparseArrays
        const Void=Nothing 
        is_linux()=Sys.islinux()
        is_windows()=Sys.iswindows()
