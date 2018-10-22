ae8de2c2bfb795b9819cc3ca290da2b699b18bb2
Vladimir Itskov <vitskov@users.noreply.github.com>
2018-10-22 16:53:02 -0400

    added the file with examples of codes..

---
+++ src/CombinatorialCodes/examples_of_codes.jl
@@ -0,0 +1,11 @@
+"""
+This simple function generates the combinatorial code of all possible codewords
+on n neurons. Currently this works only for n\in {1,2,..,32}
+Usage:
+C=FullCode(n)
+"""
+function FullCode(n::Int)::Simplicial.CombinatorialCode;
+if n<1 || n>32 ; error("need the argument to be an integer between 1 and 32"); end
+codewords= collect(powerset((1:n)));
+return CombinatorialCode(codewords)
+end
