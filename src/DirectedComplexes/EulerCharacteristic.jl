91301c933f6ebc37896507f151d6cbd5e7eaadbe
Vladimir Itskov <vitskov@users.noreply.github.com>
2018-10-24 18:40:09 -0400

    Update EulerCharacteristic.jl

--- src/DirectedComplexes/EulerCharacteristic.jl
+++ src/DirectedComplexes/EulerCharacteristic.jl
@@ -2,5 +2,8 @@
 EulerCharacteristic(P::GradedPoset)=(dot(P.Nelements[2:end], [(-1)^(k-1) for k=1:(P.dim+1)]))::Int;
 EulerCharacteristic(D::DirectedComplex)=EulerCharacteristic(GradedPoset(D))::Int;
 
+# this is the  non-reduced  Euler characteristic of a simplicial complex 
+EulerCharacteristic(D::SimplicialComplex)=EulerCharacteristic(fvector(D)[2:end]);
+
 # The following function computes the Euler characteristic of a vector of Betti numbers, assuming they start from \beta_0
 EulerCharacteristic(v::Vector{Int})=(dot(v, [(-1)^(k) for k=0:(length(v)-1)]))::Int;
