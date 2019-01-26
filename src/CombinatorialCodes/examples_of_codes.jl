6a26b557a290426c7b6d6a7fa5ad7432f4ba0a00
Vladimir Itskov <vitskov@users.noreply.github.com>
2019-01-26 02:16:58 -0500

    Update examples_of_codes.jl

--- src/CombinatorialCodes/examples_of_codes.jl
+++ src/CombinatorialCodes/examples_of_codes.jl
@@ -1,6 +1,6 @@
 """
 This simple function generates the combinatorial code of all possible codewords
-on n neurons. Currently this works only for n\in {1,2,..,32}
+on n neurons. Currently this works only for n<32
 Usage:
 C=FullCode(n)
 """
