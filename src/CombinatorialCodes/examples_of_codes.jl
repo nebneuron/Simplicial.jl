3a8c076152c7f5351fa5c5deffc949e02042dc8f
Vladimir Itskov <vitskov@users.noreply.github.com>
2019-01-26 11:40:16 -0500

    Update examples_of_codes.jl

--- src/CombinatorialCodes/examples_of_codes.jl
+++ src/CombinatorialCodes/examples_of_codes.jl
@@ -9,3 +9,18 @@ if n<1 || n>32 ; error("need the argument to be an integer between 1 and 32"); e
 codewords= collect(powerset((1:n)));
 return CombinatorialCode(codewords)
 end
+
+
+
+ """
+     BernoulliRandomCode(N, Nwords, p)
+ Draw `Nwords` i.i.d. samples from the distribution on `N` bit words given by `N` independent
+ Bernoulli trials with success probability `p`.
+ """
+ function BernoulliRandomCode(N::Int, Nwords::Int, p::Float64)
+     R = ((-rand(Nwords, N).+1) .<= p)
+     return CombinatorialCode(R)
+ end
+
+
+
