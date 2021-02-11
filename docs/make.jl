9f606da2acf1210e317f919df00346b7e03dc22a
Vladimir Itskov <vladimir.itskov@gmail.com>
2021-02-10 21:10:43 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,9 +5,7 @@ using Documenter, Simplicial
 # makedocs(modules=[Simplicial])
 makedocs(sitename="The documentation for Package Simplicial")
 
-# deploydocs(
-#    deps   = Deps.pip("mkdocs", "python-markdown-math"),
-#    repo   = "github.com/nebneuron/Simplicial.jl.git",
-#    julia  = "0.6",
-#    osname = "linux"
-#)
+ deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/nebneuron/Simplicial.jl.git",
+)
